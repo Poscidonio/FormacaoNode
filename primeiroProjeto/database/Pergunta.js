7f6e4abf442f9345a5aef6cfb7b206ff4bb134fe
Poscidonio <guilhermeposcidonio@gmail.com>
2022-09-22 22:59:37 -0300

    Mudando o Layout

--- primeiroProjeto/database/Pergunta.js
+++ primeiroProjeto/database/Pergunta.js
@@ -1,19 +1,19 @@
 //este é um model estrutura que representa as tabelas do banco de dados com express
- const Sequelize = require("sequelize")
- const connection = require("./database")
-//cria no banco uma tabela com os compos titulo e descriçcao passando 
+const Sequelize = require('sequelize');
+const connection = require('./database');
+//cria no banco uma tabela com os compos titulo e descriçcao passando
 //o tipo da variavel e se ela pode ser nula ou nao, no caso nao pode ser
- const Pergunta = connection.define('pergunta', {
-    titulo:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    descricao:{
-        type: Sequelize.TEXT,
-        allowNull: false
-    }
- })
+const Pergunta = connection.define('pergunta', {
+  titulo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  descricao: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
 //vai sincronizar com o banco de dados e nao vai forcar caso a tabela ja exista
- Pergunta.sync({force: false}).then(() =>{})
+Pergunta.sync({ force: false }).then(() => {});
 
- module.exports = Pergunta;
+module.exports = Pergunta;
--- primeiroProjeto/index.js
+++ primeiroProjeto/index.js
@@ -1,71 +1,89 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
-const connection = require("./database/database")
-const Pergunta = require("./database/Pergunta")
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta');
 //conectando ao banco
-connection 
-    .authenticate()
-    .then(() => {
-        console.log("Conexao feita com o banco de dados!")
-    })
-    .catch((msgErro) =>{
-        console.log(msgErro)
-    })
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Conexao feita com o banco de dados!');
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
+  });
 
 //Estou dizendo para o express usar o EJS como view engine
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 //codificando os dados para que o backEnd entenda
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 //permite a leitura de dados com a estrutura json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get("/", (req, res) =>{
-    //ira listar todas as perguntas na pagina inicial 
-    //raw tras apenas os dados da tabela e nada mais 
-    Pergunta.findAll({raw: true, order:[
-        ['id','DESC']//orderna de forma decrescente e ASC em ordem crescente 
-    ]}).then(pergunta =>{
-        res.render("index",{
-            pergunta: pergunta
-        })
-    })
-    
-})
+app.get('/', (req, res) => {
+  //ira listar todas as perguntas na pagina inicial
+  //raw tras apenas os dados da tabela e nada mais
+  Pergunta.findAll({
+    raw: true,
+    order: [
+      ['id', 'DESC'], //orderna de forma decrescente e ASC em ordem crescente
+    ],
+  }).then((pergunta) => {
+    res.render('index', {
+      pergunta: pergunta,
+    });
+  });
+});
 
-app.get("/perguntar",(req,res) => {
-    res.render("perguntar")
-})
+app.get('/perguntar', (req, res) => {
+  res.render('perguntar');
+});
 
-app.post("/salvarpergunta", (req, res) =>{
-    //pega os dados do formulario salva nas variaveis
-    var titulo = req.body.titulo
-    var descricao = req.body.descricao
-    //depois salva no banco 
-    Pergunta.create({
-        titulo: titulo,
-        descricao: descricao
-    }).then(() => {
-        //apos salvar redireciona para pagina inicial
-        res.redirect("/")
-    })
-})
+app.post('/salvarpergunta', (req, res) => {
+  //pega os dados do formulario salva nas variaveis
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  //depois salva no banco
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    //apos salvar redireciona para pagina inicial
+    res.redirect('/');
+  });
+});
 
-app.get("/pergunta/:id",(req, res) =>{
-var id = req.params.id;
-Pergunta.findOne({
-    where: {id: id}
-}).then(pergunta => {
-    if(pergunta != undefined){//se pergunta for diferente de vazia 
-        res.render("pergunta", {
-            pergunta: pergunta
-            
-        });
-    }else { // se a pergunta nao for encontrada
-        res.redirect("/")
+app.get('/pergunta/:id', (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: { id: id },
+  }).then((pergunta) => {
+    if (pergunta != undefined) {
+      //se pergunta for diferente de vazia
+      res.render('pergunta', {
+        pergunta: pergunta,
+      });
+    } else {
+      // se a pergunta nao for encontrada
+      res.redirect('/');
     }
-})
-})
-app.listen(3000, ()=>{console.log("app rodando!")})
+  });
+});
+//recebe os dados doo formulario
+app.post('/responder', (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId,
+  }).then(() => {
+    res.redirect('/pergunta/' + perguntaId);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('app rodando!');
+});
