8a186502205cdc929dc7aa29af899cdc2bb8477d
Poscidonio <guilhermeposcidonio@gmail.com>
2022-10-16 18:31:12 -0300

    Criando a home

--- guiaPress/articles/ArticlesController.js
+++ guiaPress/articles/ArticlesController.js
@@ -5,7 +5,9 @@ const Category = require('../categories/Category');
 const Article = require('./Article');
 
 router.get('/admin/articles', (req, res) => {
-  res.send('ROTA DE ARTIGOS');
+  Article.findAll({ include: [{ model: Category }] }).then((articles) => {
+    res.render('admin/articles/index', { articles: articles });
+  });
 });
 
 router.get('/admin/articles/new', (req, res) => {
@@ -23,10 +25,32 @@ router.post('/articles/save', (req, res) => {
     title: title,
     slug: slugify(title),
     body: body,
-    category: category,
+    categoryId: category,
   }).then(() => {
     res.redirect('/admin/articles');
   });
+});
+
+router.post('/articles/delete', (req, res) => {
+  var id = req.body.id;
+  //diferente de null
+  if (id != undefined) {
+    //NaN not a number / esta negando a validaçao se ele é um numero ou seja esta pegando o numero
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect('/admin/articles');
+      });
+    } else {
+      //se nao for um numero
+      res.redirect('/admin/articles');
+    }
+  } else {
+    res.redirect('/admin/articles');
+  }
 });
 
 module.exports = router;
