007c6349e12ebcb96f61ab0320c88b71aca1b85b
Poscidonio <guilhermeposcidonio@gmail.com>
2022-10-25 23:31:06 -0300

    Iniciando com APIs

--- apiDeGames/index.js
+++ apiDeGames/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -69,13 +72,43 @@ app.delete('/game/:id', (req, res) => {
   } else {
     var id = parseInt(req.params.id);
 
-    var index = DB.games.find((g) => g.id == id);
+    var index = DB.games.findIndex((g) => g.id == id);
 
     if (index == -1) {
       res.sendStatus(404);
     } else {
       DB.games.splice(index, 1);
       res.sendStatus(200);
+    }
+  }
+});
+
+app.put('/game/:id', (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+
+    var game = DB.games.find((g) => g.id == id);
+
+    if (game != undefined) {
+      var { title, price, year } = req.body;
+
+      if (title != undefined) {
+        game.title = title;
+      }
+
+      if (price != undefined) {
+        game.price = price;
+      }
+
+      if (year != undefined) {
+        game.year = year;
+      }
+
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
     }
   }
 });
