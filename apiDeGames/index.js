09fa02709c385af194f9b014ea95c1d767d2b4d3
Poscidonio <guilhermeposcidonio@gmail.com>
2022-10-31 17:45:56 -0300

    Iniciando o JWT

--- apiDeGames/index.js
+++ apiDeGames/index.js
@@ -2,11 +2,37 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const JWTSecret = 'AquiVoceEscreveUmTokenDesejadoAleatoriamente';
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//middleware
+function auth(req, res, next) {
+  const authToken = req.headers['authorization'];
+
+  if (authToken != undefined) {
+    const bearer = authToken.split(' ');
+    var token = bearer[1];
+
+    jwt.verify(token, JWTSecret, (err, data) => {
+      if (err) {
+        res.status(401);
+        res.json({ err: 'TOKEN inválido!' });
+      } else {
+        req.token = token;
+        req.loggedUser = { id: data.id, email: data.email };
+        next();
+      }
+    });
+  } else {
+    res.status(401);
+    res.json({ err: 'Token inválido!' });
+  }
+}
 
 //banco de dados chumbado
 var DB = {
@@ -30,14 +56,28 @@ var DB = {
       price: 5,
     },
   ],
+  users: [
+    {
+      id: 65,
+      name: 'Guilherme',
+      email: 'guilherme@gmail.com',
+      password: 'guilherme123',
+    },
+    {
+      id: 25,
+      name: 'Joao',
+      email: 'joao@gmail.com',
+      password: 'joao123',
+    },
+  ],
 };
 
-app.get('/games', (req, res) => {
+app.get('/games', auth, (req, res) => {
   res.statusCode = 200;
   res.json(DB.games);
 });
 
-app.get('/game/:id', (req, res) => {
+app.get('/game/:id', auth, (req, res) => {
   if (isNaN(req.params.id)) {
     res.sendStatus(400);
   } else {
@@ -54,7 +94,7 @@ app.get('/game/:id', (req, res) => {
   }
 });
 
-app.post('/game', (req, res) => {
+app.post('/game', auth, (req, res) => {
   var { title, price, year } = req.body;
 
   DB.games.push({
@@ -66,7 +106,7 @@ app.post('/game', (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete('/game/:id', (req, res) => {
+app.delete('/game/:id', auth, (req, res) => {
   if (isNaN(req.params.id)) {
     res.sendStatus(400);
   } else {
@@ -83,7 +123,7 @@ app.delete('/game/:id', (req, res) => {
   }
 });
 
-app.put('/game/:id', (req, res) => {
+app.put('/game/:id', auth, (req, res) => {
   if (isNaN(req.params.id)) {
     res.sendStatus(400);
   } else {
@@ -110,6 +150,36 @@ app.put('/game/:id', (req, res) => {
     } else {
       res.sendStatus(404);
     }
+  }
+});
+
+app.post('/auth', (req, res) => {
+  var { email, password } = req.body;
+
+  if (email != undefined) {
+    var user = DB.users.find((u) => u.email == email);
+    if (user != undefined) {
+      if (user.password == password) {
+        jwt.sign({ id: user.id, email: user.email }, JWTSecret, { expiresIn: '48h' }, (err, token) => {
+          if (err) {
+            res.status(400);
+            res.json({ err: 'Falha interna' });
+          } else {
+            res.status(200);
+            res.json({ token: token });
+          }
+        });
+      } else {
+        res.status(401);
+        res.json({ err: 'Credenciais!' });
+      }
+    } else {
+      res.status(404);
+      res.json({ err: 'O email enviado não existe na base de dados' });
+    }
+  } else {
+    res.status(400);
+    res.json({ err: 'O email enviado é invalido' });
   }
 });
 
