3861959cb4310b558a63fcc08f782db871455e8f
Poscidonio <guilhermeposcidonio@gmail.com>
2022-11-08 18:15:07 -0300

    Finalizando banco Knex

--- Banco_knex/index.js
+++ Banco_knex/index.js
@@ -105,3 +105,169 @@ database
   .catch((err) => {
     console.log(err);
   }); */
+
+/* DELETE 
+database
+  .where({ id: 3 })
+  .delete()
+  .table('games')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ */
+
+/* UPDATE
+database
+  .where({ id: 5 })
+  .update({ preco: 0 })
+  .table('games')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ */
+
+/* ORDERBY ordenação crescente = asc e descresente = desc
+database
+  .select()
+  .table('games')
+  .orderBy('preco', 'desc')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ */
+
+/* INSERT  relacional com isso a blizzard esta associada a um jogo no caso o id 5 que é GTA
+  database
+  .insert({
+    nome: 'Blizzard',
+    game_id: 5,
+  })
+  .table('estudios')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ */
+
+/*   innerJoin faz a consulta unindo duas tabelas porem se o campo comparado for nulo nao retorna nada 
+database
+  .select()
+  .table('games')
+  .innerJoin('estudios', 'estudios.game_id', 'games.id')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ 
+Unindo as tabelas com campos especificicos solicitados
+-----Relacionamento 1 p 1-----
+database
+  .select(['games.id', 'estudios.id as estudios_id', 'games.nome as game_nome', 'estudios.nome as estudio_nome'])
+  .table('games')
+  .innerJoin('estudios', 'estudios.game_id', 'games.id')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  unindo as tabelas com todos os campos de uma e especifico d outra
+  -----Relacionamento 1 p 1-----
+database
+  .select(['games.*', 'estudios.nome as estudio_nome'])
+  .table('games')
+  .innerJoin('estudios', 'estudios.game_id', 'games.id')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+unindo as tabelas com todos os campos de uma e especifico d outra e ainda incluindo o where para algo mais especifico
+  -----Relacionamento 1 p 1-----
+database
+  .select(['games.*', 'estudios.nome as estudio_nome'])
+  .table('games')
+  .innerJoin('estudios', 'estudios.game_id', 'games.id')
+  .where('games.id', 5)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  unindo as tabelas com todos os campos de uma e especifico d outra e ainda incluindo o where para algo mais especifico
+  -----Relacionamento 1 p N-----
+database
+  .select(['games.*', 'estudios.nome as estudio_nome'])
+  .table('games')
+  .innerJoin('estudios', 'estudios.game_id', 'games.id')
+  .where('games.id', 5)
+  .then((data) => {
+    var game = {
+      id: 0,
+      nome: '',
+      estudios: [],
+    };
+    game.id = data[0].id;
+    game.nome = data[0].nome;
+//percorre a tabela para trazer todos os registros relacionados com o join
+    data.forEach((estudio) => {
+      game.estudios.push({ nome: estudio.estudio_nome });
+    });
+    console.log(game);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+*/
+
+/* unindo as tabelas com todos os campos de uma e especifico d outra e ainda incluindo o where para algo mais especifico
+-----Relacionamento N p N-----
+database
+  .select(['estudios.nome as estudios_nome', 'games.nome as game_nome', 'games.preco'])
+  .table('games_estudios')
+  .innerJoin('games', 'games.id', 'games_estudios.game_id')
+  .innerJoin('estudios', 'estudios.id', 'games_estudios.estudio_id')
+  .where('games.id', 2)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ */
+
+//transaction
+
+async function testeTransaction() {
+  try {
+    await database.trasaction(async (trans) => {
+      await database.insert({ nome: 'Abreu' }).table('estudios');
+      await database.insert({ nome: 'Python' }).table('estudos');
+      await database.insert({ nome: 'node' }).table('estudios');
+      await database.insert({ nome: 'AWS' }).table('estudios');
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+testeTransaction();
