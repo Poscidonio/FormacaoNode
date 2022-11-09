6fc95e57e5201f31c4308f8cca627820b0fd0ac5
Poscidonio <guilhermeposcidonio@gmail.com>
2022-11-09 18:02:12 -0300

    Iniciando vueJS

---
+++ guiaclientes/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Guia clientes</h1>
+    <!-- Com Props -->
+    <Cliente :cliente="clienteProps" />
+    <!-- Sem Props -->
+    <Cliente />
+  </div>
+</template>
+
+<script>
+import Cliente from './components/Cliente';
+export default {
+  name: 'App',
+  data() {
+    return {
+      clienteProps: {
+        nome: 'Guilherme',
+        descricao: 'testando a reatividade !!',
+        email: 'guilherme@gmail.com',
+        idade: 26,
+      },
+    };
+  },
+  components: {
+    Cliente,
+  },
+};
+</script>
+
+<style></style>
---
+++ guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- aqui estamos com reatividade twoWay com leitura e escrita ou 
+seja assim que alterar ele reagiaa as alterações momentaniamente  
+em todos os lugares que estao chamando o componente-->
+  <div id="cliente">
+    <h2>{{ cliente.nome }}</h2>
+    <p>Descrição do cliente: {{ cliente.descricao }}</p>
+    <p>Email: {{ cliente.email }}</p>
+    <p>Idade: {{ cliente.idade }}</p>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    nome: String,
+    descricao: String,
+    email: String,
+    idade: Number,
+    cliente: Object,
+  },
+};
+</script>
+
+<style scoped>
+#cliente {
+  color: blue;
+}
+</style>
