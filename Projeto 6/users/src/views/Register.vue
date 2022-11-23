7db498e2ad68e2935d33f1369f113f67de2ce5b2
Poscidonio <guilhermeposcidonio@gmail.com>
2022-11-23 17:28:27 -0300

    Reajustandos os erros que estavam acontecendo.

--- Projeto 6/users/src/views/Register.vue
+++ Projeto 6/users/src/views/Register.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
     <h2>Registro de usuário !</h2>
+    <hr />
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <input
+          type="text"
+          placeholder="Nome do usuário"
+          class="input"
+        />
+        <p>Email</p>
+        <input
+          type="email"
+          placeholder="email@email.com"
+          class="input"
+        />
+        <p>Senha</p>
+        <input
+          type="password"
+          placeholder="******"
+          class="input"
+        />
+        <hr />
+        <button class="button is-sucess">Cadastrar</button>
+      </div>
+    </div>
   </div>
 </template>
 
