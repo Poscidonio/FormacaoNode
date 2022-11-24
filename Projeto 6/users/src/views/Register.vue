ee59aa83ea4f59ab66f62d57c49d0d8a3ab9153d
Poscidonio <guilhermeposcidonio@gmail.com>
2022-11-24 15:33:04 -0300

    Projeto 6 continuação

--- Projeto 6/users/src/views/Register.vue
+++ Projeto 6/users/src/views/Register.vue
@@ -4,32 +4,60 @@
     <hr />
     <div class="columns is-centered">
       <div class="column is-half">
+        <p>Nome</p>
         <input
           type="text"
           placeholder="Nome do usuário"
           class="input"
+          v-model="name"
         />
         <p>Email</p>
         <input
           type="email"
           placeholder="email@email.com"
           class="input"
+          v-model="email"
         />
         <p>Senha</p>
         <input
           type="password"
           placeholder="******"
           class="input"
+          v-model="password"
         />
         <hr />
-        <button class="button is-sucess">Cadastrar</button>
+        <button
+          class="button is-sucess"
+          @click="register"
+        >
+          Cadastrar
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: '',
+      password: '',
+      email: '',
+    };
+  },
+  methods: {
+    register() {
+      axios
+        .get('http://localhost:8686/', (res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
