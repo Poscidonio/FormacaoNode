5fe046a6c541055f5f48958ed7bda3afd31e6b4b
Poscidonio <guilhermeposcidonio@gmail.com>
2022-12-06 23:52:44 -0300

    continuando mongoDB

--- agendamento/README.md
+++ agendamento/README.md
@@ -6,5 +6,16 @@ npm install body-parser --save
 
 npm install mongoose --save
 
-criar no mongoDB
-~use agendamento
+---------------- MongoDB ----------------
+
+~mongo - abre o servidor
+
+~mongod - executa o servidor se ele nao estiver executando
+
+~show dbs - mostra os bancos
+
+~use agendamento - criar no mongoDB
+
+~show collections - mostras as collections
+
+~db.nomeDaColletions.find() - mostra os dados armazenados
--- agendamento/factories/AppointmentFactory.js
+++ agendamento/factories/AppointmentFactory.js
@@ -3,16 +3,20 @@ class AppointmentFactory {
     var day = simpleAppointment.date.getDate() + 1; //retorna o dia certo porque o getDay retorna outra coisa, o +1 é porque a data ta meia noite ele pega o dia anterior
     var month = simpleAppointment.date.getMonth();
     var year = simpleAppointment.date.getFullYear();
-
+    //split - separador posição antes e depois dos :
     var hour = simpleAppointment.time.split(':')[0];
     var minutes = simpleAppointment.time.split(':')[1];
 
+    var startDate = new Date(year, month, day, hour, minutes, 0, 0);
+    //time zone
+    startDate.setHours(startDate.getHours() - 3);
     var appo = {
       id: simpleAppointment._id,
       title: simpleAppointment.name + ' - ' + simpleAppointment.description,
-      start: undefined,
-      end: undefined,
+      start: startDate,
+      end: startDate,
     };
+    return appo;
   }
 }
 
