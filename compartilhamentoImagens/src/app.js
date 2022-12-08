252c2760705d43d15d73b7cc0ba0b7b7cebb2ee7
Poscidonio <guilhermeposcidonio@gmail.com>
2022-12-08 18:13:22 -0300

    continuação de testes

--- compartilhamentoImagens/src/app.js
+++ compartilhamentoImagens/src/app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
+let user = require('./models/User');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -14,8 +15,24 @@ mongoose
     console.log(err);
   });
 
+let User = mongoose.model('User', user);
+
 app.get('/', (req, res) => {
   res.json({});
+});
+
+app.post('/user', async (req, res) => {
+  if (req.body.name == '' || req.body.email == '' || req.body.password == '') {
+    res.sendStatus(400);
+    return;
+  }
+  try {
+    let newUser = new User({ name: req.body.name, email: req.body.email, password: req.body.password });
+    await newUser.save();
+    res.json({ email: req.body.email });
+  } catch (err) {
+    res.sendStatus(500);
+  }
 });
 
 module.exports = app;
--- compartilhamentoImagens/test/user.test.js
+++ compartilhamentoImagens/test/user.test.js
@@ -20,4 +20,47 @@ describe('Cadastro de usuario', () => {
         fail(err);
       });
   });
+
+  test('Deve impedir que um usuario se cadastre com os dados  vazios', () => {
+    let user = { name: '', email: '', password: '' };
+
+    return request
+      .post('/user')
+      .send(user)
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+      })
+      .catch((err) => {
+        fail(err);
+      });
+  });
+
+  test('Deve impedir o usuario cadastrar com um email existente', () => {
+    //criando como um email novo
+    let time = Date.now();
+    let email = `${time}@gmail.com`;
+    let user = { name: 'Guilherme', email: email, password: '123456' };
+
+    return request
+      .post('/user')
+      .send(user)
+      .then((res) => {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.email).toEqual(email);
+
+        request
+          .post('/user')
+          .send(user)
+          .then((res) => {
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.email).toEqual(email);
+          })
+          .catch((err) => {
+            fail(err);
+          });
+      })
+      .catch((err) => {
+        fail(err);
+      });
+  });
 });
